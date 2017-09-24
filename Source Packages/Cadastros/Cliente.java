af0b4ce7c21645a5b6f2f04693e21a57449c8bc6
Nilo Alves <niloalves@hotmail.com>
2017-09-24 00:15:04 -0300

    Implementando a classe Fornecedor

--- Source Packages/Cadastros/Cliente.java
+++ Source Packages/Cadastros/Cliente.java
@@ -1,19 +1,9 @@
 package Cadastros;
 
-
 import Repositorio.BaseDados;
 import java.util.HashMap;
 import java.util.Objects;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author vitor
- */
 public class Cliente {
     
     private String nome;
--- Source Packages/Cadastros/Fornecedor.java
+++ Source Packages/Cadastros/Fornecedor.java
@@ -1,6 +1,127 @@
-
 package Cadastros;
 
+import Repositorio.BaseDados;
+import java.util.HashMap;
+
 public class Fornecedor {
+    
+    private String nome;
+    private int cnpj;
+    private int numero;
+    private int cep;
+    private String complemento;
+    private String bairro;
+    private int telefone;
+    private String cidade;
+    private String unidade_federativa;
+    private int inscricao_e;
+    private int inscricao_m;
+    private String contato;
+    private String email;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(int cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getCep() {
+        return cep;
+    }
+
+    public void setCep(int cep) {
+        this.cep = cep;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUnidade_federativa() {
+        return unidade_federativa;
+    }
+
+    public void setUnidade_federativa(String unidade_federativa) {
+        this.unidade_federativa = unidade_federativa;
+    }
+
+    public int getInscricao_e() {
+        return inscricao_e;
+    }
+
+    public void setInscricao_e(int inscricao_e) {
+        this.inscricao_e = inscricao_e;
+    }
+
+    public int getInscricao_m() {
+        return inscricao_m;
+    }
+
+    public void setInscricao_m(int inscricao_m) {
+        this.inscricao_m = inscricao_m;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
     
 }
