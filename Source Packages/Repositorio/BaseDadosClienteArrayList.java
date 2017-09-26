fd0015b8a443a4fe3b05a60f8ff7c60722fc6da5
ViniciusLima5 <vinicius.lalmeida98@gmail.com>
2017-09-26 09:39:09 -0300

    Faltando apenas função alterar no Cliente, além de refinamentos no form.

--- Source Packages/Repositorio/BaseDadosClienteArrayList.java
+++ Source Packages/Repositorio/BaseDadosClienteArrayList.java
@@ -4,11 +4,19 @@
  * and open the template in the editor.
  */
 package Repositorio;
+import Cadastros.ClienteArrayList;
+import java.util.ArrayList;
 
 /**
  *
  * @author Vinícius
  */
 public class BaseDadosClienteArrayList {
-    
+    private static ArrayList<ClienteArrayList> clientes;
+     public static ArrayList<ClienteArrayList> getClientes () {
+        if (clientes==null) {
+            clientes = new ArrayList<>();
+        }
+        return clientes;
+    }
 }
