0b655b7866471e1cc00f24993fab00fc0f2abb81
ViniciusLima5 <vinicius.lalmeida98@gmail.com>
2017-09-26 11:33:49 -0300

    Melhoria no formulário. Adição de controle para CPF e Telefone

--- Source Packages/Cadastros/ClienteArrayList.java
+++ Source Packages/Cadastros/ClienteArrayList.java
@@ -16,6 +16,10 @@ public class ClienteArrayList {
     private String email;
     private String telefone;
     private String rua;
+    private String estado;
+    private String cidade;
+    private String numero;
+    private String complemento;
 
     public String getNome() {
         return nome;
@@ -64,5 +68,38 @@ public class ClienteArrayList {
     public void setRua(String rua) {
         this.rua = rua;
     }
-    
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+   
 }
